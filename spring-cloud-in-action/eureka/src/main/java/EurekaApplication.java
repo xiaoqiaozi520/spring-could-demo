c26d0da3ad464a9195a73620ddc26d1242448586
heqiao <qhe@wisepool.com.cn>
2019-03-04 17:34:19 +0800

    Cres

---
+++ spring-cloud-in-action/eureka/src/main/java/EurekaApplication.java
@@ -0,0 +1,14 @@
+@SpringBootApplication
+@RestController
+public class EurekaApplication.java {
+
+    @RequestMapping("/")
+    public String home() {
+        return "Hello world";
+    }
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(Application.class).web(true).run(args);
+    }
+
+}
